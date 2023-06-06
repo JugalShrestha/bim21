893da9d285d0f95e9ec6d49d299b50107db42ed3
Jugal Shrestha <jugalnepali@gmail.com>
2023-06-06 15:26:32 +0545

    Updated routine (A+B)

--- Scripts/class-routine-js/routine.js
+++ Scripts/class-routine-js/routine.js
@@ -1,8 +1,10 @@
 const routineDayHeader = document.querySelectorAll('.routine-day')
+const routineDayHeaderB = document.querySelectorAll('.routine-day-b')
 const date = new Date()
 const dayOfTheWeek = date.getDay()
 //Sunday = 0 . Sat = 6
 if(dayOfTheWeek!=6)
 {
     routineDayHeader[dayOfTheWeek].style.background ="var(--n2-color)"
+    routineDayHeaderB[dayOfTheWeek].style.background ="var(--n2-color)"
 }
