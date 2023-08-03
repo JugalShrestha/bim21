54745406673bab2f867a3b242dee5a87127eec6a
Jugal Shrestha <jugalnepali@gmail.com>
2023-08-03 14:11:37 +0545

    Fixed Pratik Name in Text2Hw

--- Scripts/text-2-handwriting-js/text-2-handwriting.js
+++ Scripts/text-2-handwriting-js/text-2-handwriting.js
@@ -20,7 +20,7 @@ const handwritings = [
         font: "handwriting-12.ttf",
     },
     {
-        name: "30 . Prateek Raut",
+        name: "30 . Pratik Raut",
         id: "30",
         fontName: "handwriting-30",
         font: "handwriting-30.ttf",
