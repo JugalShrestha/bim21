586d564949fc57b2ca8aa45eb5a57f7d6c00e746
Jugal Shrestha <jugalnepali@gmail.com>
2023-12-25 14:51:01 +0545

    Add: new Routine

--- src/pages/project-pages/Routine.tsx
+++ src/pages/project-pages/Routine.tsx
@@ -6,22 +6,20 @@ const Routine = () => {
   const dayOfTheWeek = date.getDay();
 
   useEffect(() => {
-    const routineDayHeaders = document.querySelectorAll(".routine-day");
-    const routineDayHeadersB = document.querySelectorAll(".routine-day-b");
+    const routineRow = document.querySelectorAll(".routine-section-A tr");
+    const routineRowB = document.querySelectorAll(".routine-section-B tr");
 
-    if (dayOfTheWeek !== 6 && dayOfTheWeek > 0) {
-      const routineDayHeader = routineDayHeaders[
-        dayOfTheWeek - 1
-      ] as HTMLElement;
-      const routineDayHeaderB = routineDayHeadersB[
-        dayOfTheWeek - 1
-      ] as HTMLElement;
-
-      routineDayHeader.style.background = "var(--n2-color)";
-      routineDayHeaderB.style.background = "var(--n2-color)";
+    if (dayOfTheWeek !== 6 && dayOfTheWeek >= 0) {
+      routineRow.forEach((r) => {
+        r.classList.remove("activeDay");
+      });
+      routineRowB.forEach((r) => {
+        r.classList.remove("activeDay");
+      });
+      routineRow[dayOfTheWeek + 2].classList.add("activeDay");
+      routineRowB[dayOfTheWeek + 2].classList.add("activeDay");
     }
   }, [dayOfTheWeek]);
-
   return (
     <section className="class-routine-page" id="page">
       <GoBackBtn />
@@ -29,172 +27,127 @@ const Routine = () => {
         <div className="header">
           <div className="main-header">Class Routine</div>
           <div className="sub-header">
-            Class routine for BIM 3rd Sem section A
+            Class routine for section A and section B
           </div>
         </div>
         <div className="class-routine-body">
-          <table className="routine">
+          <table className="routine-section-A">
             <thead>
-              <tr className="routine-row">
-                <td className="routine-cell routine-base" colSpan={2}>
-                  Time
-                </td>
-                <td className="routine-cell routine-header routine-day">Mon</td>
-                <td className="routine-cell routine-header routine-day">Tue</td>
-                <td className="routine-cell routine-header routine-day">Wed</td>
-                <td className="routine-cell routine-header routine-day">Thu</td>
-                <td className="routine-cell routine-header routine-day">Fri</td>
+              <tr>
+                <th className="section-cell" colSpan={5}>
+                  Section A
+                </th>
+              </tr>
+              <tr>
+                <th>Days</th>
+                <th>6:35 - 7:45</th>
+                <th>7:55 - 8:30</th>
+                <th>9:30 - 11:00</th>
+                <th>11:10 - 12:40</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="routine-row">
-                <td rowSpan={2} className="routine-cell routine-time">
-                  6:15
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">FA</td>
-                <td className="routine-cell">FA</td>
-                <td className="routine-cell">WTI</td>
-                <td className="routine-cell">MPCA</td>
-                <td className="routine-cell">WTI</td>
+              <tr>
+                <td>Sun</td>
+                <td>EB</td>
+                <td>Web Tech II</td>
+                <td>OS</td>
+                <td>BDCCN</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">Lab-B</td>
-                <td className="routine-cell room-no">Lab-B</td>
-                <td className="routine-cell room-no">424</td>
+              <tr>
+                <td>Mon</td>
+                <td>Web Tech II</td>
+                <td>DBMS</td>
+                <td>CMA</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td rowSpan={2} className="routine-cell routine-time">
-                  7:45
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">DSA</td>
-                <td className="routine-cell">DSA</td>
-                <td className="routine-cell">DSA</td>
-                <td className="routine-cell">WTI</td>
-                <td className="routine-cell">B.Stat</td>
+              <tr>
+                <td>Tue</td>
+                <td>EB</td>
+                <td>DBMS</td>
+                <td>BDCCN</td>
+                <td>CMA</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">Lab-D</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
+              <tr>
+                <td>Wed</td>
+                <td>EB</td>
+                <td>OS</td>
+                <td>CMA</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td rowSpan={2} className="routine-cell routine-time">
-                  9:30
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">B.Stat</td>
-                <td className="routine-cell">B.Stat</td>
-                <td className="routine-cell">MPCA</td>
-                <td className="routine-cell">FA</td>
-                <td className="routine-cell">MPCA</td>
+              <tr>
+                <td>Thur</td>
+                <td>Web Tech II</td>
+                <td>OS</td>
+                <td>-</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">Lab-D</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
-                <td className="routine-cell room-no">424</td>
+              <tr>
+                <td>Fri</td>
+                <td>DBMS</td>
+                <td>BDCCN</td>
+                <td>-</td>
+                <td>-</td>
               </tr>
             </tbody>
           </table>
-        </div>
-      </div>
-      <div className="routine-whole">
-        <div className="header">
-          <div className="sub-header">
-            Class routine for BIM 3rd Sem section B
-          </div>
-        </div>
-        <div className="class-routine-body">
-          <table className="routine">
+          <table className="routine-section-B">
             <thead>
-              <tr className="routine-row">
-                <td className="routine-cell routine-base" colSpan={2}>
-                  Time
-                </td>
-                <td className="routine-cell routine-header routine-day-b">
-                  Mon
-                </td>
-                <td className="routine-cell routine-header routine-day-b">
-                  Tue
-                </td>
-                <td className="routine-cell routine-header routine-day-b">
-                  Wed
-                </td>
-                <td className="routine-cell routine-header routine-day-b">
-                  Thu
-                </td>
-                <td className="routine-cell routine-header routine-day-b">
-                  Fri
-                </td>
+              <tr>
+                <th className="section-cell" colSpan={5}>
+                  Section B
+                </th>
+              </tr>
+              <tr>
+                <th>Days</th>
+                <th>6:35 - 7:45</th>
+                <th>7:55 - 8:30</th>
+                <th>9:30 - 11:00</th>
+                <th>11:10 - 12:40</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="routine-row">
-                <td className="routine-cell routine-time" rowSpan={2}>
-                  6:15
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">B.Stat</td>
-                <td className="routine-cell">MPCA</td>
-                <td className="routine-cell">FA</td>
-                <td className="routine-cell">FA</td>
-                <td className="routine-cell">DSA</td>
+              <tr>
+                <td>Sun</td>
+                <td>Web Tech II</td>
+                <td>EB</td>
+                <td>CMA</td>
+                <td>OS</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">425</td>
+              <tr>
+                <td>Mon</td>
+                <td>DBMS</td>
+                <td>Web Tech II</td>
+                <td>BDCCN</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-time" rowSpan={2}>
-                  7:45
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">WTI</td>
-                <td className="routine-cell">WTI</td>
-                <td className="routine-cell">WTI</td>
-                <td className="routine-cell">DSA</td>
-                <td className="routine-cell">FA</td>
+              <tr>
+                <td>Tue</td>
+                <td>EB</td>
+                <td>OS</td>
+                <td>CMA</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">Lab-B</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">Lab-D</td>
-                <td className="routine-cell room-no">425</td>
+              <tr>
+                <td>Wed</td>
+                <td>OS</td>
+                <td>DBMS</td>
+                <td>BDCCN</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-time" rowSpan={2}>
-                  9:30
-                </td>
-                <td className="routine-cell routine-header">Sub</td>
-                <td className="routine-cell">MPCA</td>
-                <td className="routine-cell">DSA</td>
-                <td className="routine-cell">B.Stat</td>
-                <td className="routine-cell">MPCA</td>
-                <td className="routine-cell">B.Stat</td>
+              <tr>
+                <td>Thur</td>
+                <td>CMA</td>
+                <td>Web Tech II</td>
+                <td>BDCCN</td>
+                <td>-</td>
               </tr>
-              <tr className="routine-row">
-                <td className="routine-cell routine-header">Room</td>
-                <td className="routine-cell room-no">Lab-B</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">Lab-D</td>
-                <td className="routine-cell room-no">425</td>
-                <td className="routine-cell room-no">425</td>
+              <tr>
+                <td>Fri</td>
+                <td>EB</td>
+                <td>DBMS</td>
+                <td>-</td>
+                <td>-</td>
               </tr>
             </tbody>
           </table>
